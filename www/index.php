5b56ba959d6ddf0bfd493a085e79838abe493eef
Juan A. Ripoll <129286637+juanantonioripoll@users.noreply.github.com>
2023-11-14 16:27:22 +0100

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubuntu Server Docker Lamp By Juan A. Ripoll</title>
+</head>
+
+<body>
+    <div style="text-align: center; margin: 20px 20px 20px 20px;">
+        <h1 style=" font-size:2em;">Ubuntu Server Docker Lamp</h1>
+        <a href="https://www.juanantonioripoll.es" title="Juan A. Ripoll" target="_blank" style="background:darkorange; color:#080808; border-radius:30px; padding: 15px 15px 15px 15px;">https://www.juanantonioripoll.es</a>
+        <div style="margin-top: 50px;"></div>
+        <?php
+        phpinfo();
+        ?>
+    </div>
+
+
+</body>
+
+</html>
